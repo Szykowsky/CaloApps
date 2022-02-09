a8d31140fda5125e1af62a824aa0b5124740c7af
Kacper Szymanek <34415093+Szykowsky@users.noreply.github.com>
2022-02-09 18:26:49 +0100

    [FEATURE] Add get meals query

--- CaloApps/Controllers/MealController.cs
+++ CaloApps/Controllers/MealController.cs
@@ -1,4 +1,5 @@
 ﻿using CaloApps.Meals.Commands;
+using CaloApps.Meals.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,13 @@ namespace CaloApps.Controllers
         public async Task<IActionResult> AddMealAsync([FromBody] AddMeal.Command addMealCommand) 
         {
             var result = await this.mediator.Send(addMealCommand);
+            return Ok(result);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetMeals([FromRoute] Guid userId)
+        {
+            var result = await this.mediator.Send(new GetMeals.Query { UserId = userId });
             return Ok(result);
         }
     }
---
+++ CaloApps/Meals/Queries/GetMeals.cs
@@ -0,0 +1,45 @@
+﻿using CaloApps.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaloApps.Meals.Queries
+{
+    public class GetMeals
+    {
+        public class Query : IRequest<IEnumerable<MealDto>>
+        {
+            public Guid UserId { get; set; }
+        }
+
+        public class MealDto
+        {
+            public Guid Id { get; set; }
+            public int Kcal { get; set; }
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+
+        }
+        public class Handler : IRequestHandler<Query, IEnumerable<MealDto>>
+        {
+            private readonly CaloContext dbContext;
+
+            public Handler(CaloContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+            public async Task<IEnumerable<MealDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await this.dbContext.Meals
+                    .Where(m => m.UserId == request.UserId)
+                    .Select(m => new MealDto
+                    {
+                        Kcal = m.Kcal,
+                        Name = m.Name,
+                        Date = m.Date,
+                        Id = m.Id
+                    })
+                    .ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
