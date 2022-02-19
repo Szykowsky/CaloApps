451973228b46b1badfe259064241775902af35cd
Kacper Szymanek <34415093+Szykowsky@users.noreply.github.com>
2022-02-19 20:38:19 +0100

    (WIP)[IMPROVEMENS] Add basic backend validatiation - add meal comand

--- CaloApps/Meals/Commands/AddMeal.cs
+++ CaloApps/Meals/Commands/AddMeal.cs
@@ -1,18 +1,44 @@
 ﻿using CaloApps.Data;
 using CaloApps.Data.Models;
 using CaloApps.Shared.Models;
+using FluentValidation;
 using MediatR;
 
 namespace CaloApps.Meals.Commands
 {
     public class AddMeal
     {
-        public class Command: IRequest<RequestStatus>
+        public class Command : IRequest<RequestStatus>
         {
             public int Kcal { get; set; }
             public string Name { get; set; }
             public DateTime Date { get; set; }
             public Guid DietId { get; set; }
+        }
+
+        public class GetMealsValidator : AbstractValidator<Command>
+        {
+            public GetMealsValidator()
+            {
+                RuleFor(x => x.DietId)
+                    .NotEmpty()
+                    .NotNull()
+                    .WithMessage("You have to add diet id");
+
+                RuleFor(x => x.Name)
+                    .NotEmpty()
+                    .NotNull()
+                    .WithMessage("You Have to add name of meal")
+                    .MaximumLength(250)
+                    .WithMessage("Max length: 250 characters");
+
+                RuleFor(x => x.Kcal)
+                    .NotEmpty()
+                    .NotNull()
+                    .WithMessage("You Have to add kcal")
+                    .GreaterThanOrEqualTo(0)
+                    .WithMessage("Kcal must be grather than 0");
+            }
         }
 
         public class Handler : IRequestHandler<Command, RequestStatus>
