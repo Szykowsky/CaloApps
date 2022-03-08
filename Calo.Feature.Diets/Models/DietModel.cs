356d5dd7dfb00119edd08467f92113b128a63ae5
Kacper Szymanek <34415093+Szykowsky@users.noreply.github.com>
2022-03-08 20:14:53 +0100

    [FEATURE] Get user it from context in diet controller

---
+++ Calo.Feature.Diets/Models/DietModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Calo.Feature.Diets.Models
+{
+    public class DietModel
+    {
+        public class Basic
+        {
+            public string Name { get; set; }
+            public int DayKcal { get; set; }
+            public int? Carbohydrates { get; set; }
+            public int? Fiber { get; set; }
+            public int? Protein { get; set; }
+            public int? Fats { get; set; }
+            public int? Vitamins { get; set; }
+            public int? Minerals { get; set; }
+        }
+    }
+}
--- CaloApps/Controllers/DietController.cs
+++ CaloApps/Controllers/DietController.cs
@@ -1,35 +1,52 @@
 ﻿using Calo.Feature.Diets.Commands;
+using Calo.Feature.Diets.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CaloApps.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class DietController : ControllerBase
     {
         private readonly IMediator mediator;
-        public DietController(IMediator mediator)
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public DietController(IMediator mediator, IHttpContextAccessor httpContextAccessor)
         {
             this.mediator = mediator;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddDietAsync([FromBody] AddDiet.Command addDietCommand)
+        public async Task<IActionResult> AddDietAsync([FromBody] DietModel.Basic model)
         {
-            var result = await this.mediator.Send(addDietCommand);
+            var result = await this.mediator.Send(new AddDiet.Command
+            {
+                Carbohydrates = model.Carbohydrates,
+                DayKcal = model.DayKcal,
+                Fats = model.Fats,
+                Fiber = model.Fiber,
+                Minerals = model.Minerals,
+                Name = model.Name,
+                Protein = model.Protein,
+                Vitamins = model.Vitamins,
+                UserId = Guid.Parse(this.httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value)
+            });
             return Ok(result);
         }
 
         [Authorize]
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetDietsAsync([FromRoute] Guid userId)
+        [HttpGet]
+        public async Task<IActionResult> GetDietsAsync()
         {
             var result = await this.mediator.Send(new GetDiets.Query
             {
-                UserId = userId
+                UserId = Guid.Parse(this.httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value)
             });
             return Ok(result);
         }
